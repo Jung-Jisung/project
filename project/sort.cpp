120edbc784e0ff4f5fbefb02d44462d65696b688
Jung-Jisung <gsgs1416@snu.ac.kr>
2020-05-04 11:48:55 +0900

    hello

---
+++ project/sort.cpp
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+void swap(int *a, int *b)
+{
+  int temp = (*a);
+  (*a) = (*b);
+  (*b) = temp;
+}
+
+
+void selectionSort(int *arr, int num)
+{
+  // Implement here
+}
+
+
+void bubbleSort(int *arr, int num)
+{
+  // Implement here
+}
+
+
+void insertionSort(int *arr, int num)
+{
+  // Implement here
+}
+
+
+void mergeSort(int* arr, int left, int right, int num) 
+{
+  // Implement here
+}
+
+
+void merge(int *arr, int left, int mid, int right, int num)
+{
+  // Implement here
+}
+
